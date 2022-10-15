d6765bd0e4979e6c5b4468f6479edb75ca92def0
Vaughn <257525m@student.gla.ac.uk>
2022-10-15 18:08:57 +0100

    Fixed database

--- Team07/guitar/models.py
+++ Team07/guitar/models.py
@@ -15,9 +15,6 @@ class SpotifyToken(models.Model):
     expires_in = models.DateTimeField()
     token_type = models.CharField(max_length = 50, default='')
 
-    def __str__(self):
-        return self.name
-
 
 class Guitar(models.Model):
     skU_ID = models.CharField(max_length=64, unique = True, primary_key = True)
@@ -39,38 +36,11 @@ class Guitar(models.Model):
     createdOn = models.DateTimeField(null=True)
     imageUrls = models.URLField(null=True)
 
-    def __str__(self):
-        return self.name
-
 
 class GuitarsWithSong(models.Model):
-    skU_ID = models.OneToOneField(Guitar, on_delete=models.CASCADE, primary_key=True)
+    skU_ID = models.ForeignKey(Guitar, on_delete=models.CASCADE, primary_key=True)
     spotifyId = models.CharField(max_length = 128)
     youtubeUrl = models.URLField()
-
-    def __str__(self):
-        return self.name
-
-
-class Guitar(models.Model):
-    sku_id = models.CharField(max_length=64)
-    asn = models.CharField(default="", max_length=64)
-    category = models.CharField(max_length = 64)
-    online = models.BooleanField(default = True)
-    itemName = models.CharField(max_length = 64)
-    title = models.CharField(default="", max_length=64)
-    brandName = models.CharField(max_length = 64)
-    description = models.CharField(default = "", max_length = 256)
-    productDetail = models.CharField(max_length = 512)
-    salesPrice = models.FloatField()
-    pictureMain = models.URLField(unique=True)
-    qtyInStock = models.IntegerField(default = 0)
-    qtyOnOrder = models.IntegerField(default = 0)
-    colour = models.IntegerField(default = 0)
-    pickup = models.IntegerField(default = 0)
-    bodyShape = models.IntegerField(default = 0)
-    createOn = models.DateTimeField()
-    imageUrls = models.URLField(unique=True)
 
 
 
