1f5403bc0f22248906f3d83526d1ef574a656d11
Bobby Ayvazov <bayvazov94@gmail.com>
2022-10-15 09:55:43 +0100

    Spotify Stuff

--- Team07/Team07/urls.py
+++ Team07/Team07/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include("guitar.urls"))
+    path('guitar/', include("guitar.urls"))
 ]
--- Team07/guitar/urls.py
+++ Team07/guitar/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from .views import AuthURL, spotify_callback, IsAuthenticated
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path('get-auth-url', AuthURL.as_view()),
+    path('redirect', spotify_callback),
+    path('is-authenticated', IsAuthenticated.as_view())
 ]
