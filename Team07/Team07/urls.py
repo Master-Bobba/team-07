783c4959287916fd143f149b3f1de3c964183266
Bobby Ayvazov <bayvazov94@gmail.com>
2022-10-16 11:10:56 +0100

    almost there

--- Team07/Team07/urls.py
+++ Team07/Team07/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('guitar/', include("guitar.urls")),
+    path('', include("guitar.urls"))
     
 ]
